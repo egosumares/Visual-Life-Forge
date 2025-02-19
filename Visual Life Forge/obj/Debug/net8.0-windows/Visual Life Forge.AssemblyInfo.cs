09f977fe3c1b7c08a6c9c8a4ee261436416e0f25
egosumares <frankinstyne3@gmail.com>
2025-02-19 16:02:14 +0000

    add files

--- Visual Life Forge/obj/Debug/net8.0-windows/Visual Life Forge.AssemblyInfo.cs
+++ Visual Life Forge/obj/Debug/net8.0-windows/Visual Life Forge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Visual Life Forge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74e46d194b9399f31ba31fd1e04f6787a5f45aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Visual Life Forge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Visual Life Forge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
